0d103fbec8652e44b656a2c4be23150a23ba1868
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-16 14:23:49 +0300

    validator paketi projeye dahil edildi, validate isEmail eklendi ve hataYakala fonksiyonu yazıldı

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,10 @@
 const User = require('../models/User')
 
+//valid hatalarını yakalayacağımız fonksiyon
+const hataYakala = (err) => {
+    console.log(err.message,err.code);
+} 
+
 //signup get işlemi için controller
 module.exports.signup_get = (req,res) => {
     res.render('signup')
@@ -21,7 +26,8 @@ module.exports.signup_post = async (req,res) => {
         const user = await User.create({email,parola})
         res.status(201).json(user)
     }catch(error){
-        res.status(400).send('hata oluştu, kullanıcı oluşturulamadı ' + error)
+        //res.status(400).send('hata oluştu, kullanıcı oluşturulamadı ' + error)
+        hataYakala(error)
     }
     //res.send('yeni kullanıcı olusturuldu')
 }
--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
+const {isEmail} = require('validator')
+
 const userSchema = new mongoose.Schema({
     email:{
         type:String,
         required:[true,'Mail adresini girmeniz zorunludur'], //mongoose validation
         unique:true,
-        lowercase:true
+        lowercase:true,
+        validate:[isEmail,'Lütfen geçerli bir email adresi giriniz']
     },
     parola:{
         type:String,
