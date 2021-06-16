97893f4143bb7531a9207edb929df058b0cfba84
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-16 16:53:51 +0300

    mongoose ile hata yakalama işlemi, properties.path , user validation failed

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,24 @@ const User = require('../models/User')
 
 //valid hatalarını yakalayacağımız fonksiyon
 const hataYakala = (err) => {
-    console.log(err.message,err.code);
+
+    let errors = {email:'',parola:''}
+  
+    //err.code bilgisi ile hata yakalama
+    if(err.code === 11000){
+        errors.email = 'Bu mail adresi daha önce kayıt edilmiş!'
+        return errors
+    }
+
+    //err.message içerisinde geçen ifadeden hata yakalama
+    if(err.message.includes('user validation failed')){
+        //hatalar içerisinde dönelim
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors
+    
 } 
 
 //signup get işlemi için controller
@@ -27,7 +44,8 @@ module.exports.signup_post = async (req,res) => {
         res.status(201).json(user)
     }catch(error){
         //res.status(400).send('hata oluştu, kullanıcı oluşturulamadı ' + error)
-        hataYakala(error)
+        const errors = hataYakala(error)
+        res.status(400).json({errors})
     }
     //res.send('yeni kullanıcı olusturuldu')
 }
